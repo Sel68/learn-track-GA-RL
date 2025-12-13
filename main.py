4b0f4685e869749aec04fda94edf6c758c56eb13
sel68 <saadhasanellahie@gmail.com>
2025-12-13 18:48:44 +0500

    add NN for GA

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import os
+import torch.nn as nn
+
+CONFIG = {
+
+}
+
+class EvolutionNet(nn.Module):
+    def __init__(self):
+        super(EvolutionNet, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(CONFIG["N_SENSORS"], 16),
+            nn.ReLU(),
+            nn.Linear(16, 16),
+            nn.ReLU(),
+            nn.Linear(16, 2), # Steering, Accel
+            nn.Tanh() # contraint between -1 and 1
+        )
+        
+    def forward(self, x):
+        return self.fc(x)
